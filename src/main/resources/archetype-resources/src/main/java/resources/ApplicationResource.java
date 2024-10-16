38ee5861b8cfaf96846e18b616a21d05ab887df1
Diego Silva <diego.silval@gmail.com>
2024-10-16 16:28:30 -0500

    Proyecto base. Debería ser modulado

---
+++ src/main/resources/archetype-resources/src/main/java/resources/ApplicationResource.java
@@ -0,0 +1,9 @@
+package ${package}.resources;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("${resources-path}")
+public class ApplicationResource extends Application {
+
+}
---
+++ src/main/resources/archetype-resources/src/test/java/test/AppTest.java
@@ -0,0 +1,13 @@
+package ${package}.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertEquals(4, 2 + 2);
+    }
+}
